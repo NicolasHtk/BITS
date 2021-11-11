8a8ace174366e71e366b6225e341bbcfb32ac188
NicolasHtk <Nicolas1325@hotmail.com>
2021-11-10 19:38:31 -0500

    Implementamos React en el proyecto

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Footer from './components/Footer';
+import Main from './components/Main';
+
+function App() {
+  return (
+    <div>
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <div>
+            <div className="bg-dark text-center text-white">
+                <div className="container p-4 pb-0">
+                    <section className="mb-4">
+                        <a
+                            className="btn btn-outline-light btn-floating m-1"
+                            href="#!"
+                            role="button"
+                        >
+                            <i className="fab fa-facebook-f"></i>
+                        </a>
+
+                        <a
+                            className="btn btn-outline-light btn-floating m-1"
+                            href="#!"
+                            role="button"
+                        >
+                            <i className="fab fa-twitter"></i>
+                        </a>
+
+                        <a
+                            className="btn btn-outline-light btn-floating m-1"
+                            href="#!"
+                            role="button"
+                        >
+                            <i className="fab fa-google"></i>
+                        </a>
+
+                        <a
+                            className="btn btn-outline-light btn-floating m-1"
+                            href="#!"
+                            role="button"
+                        >
+                            <i className="fab fa-instagram"></i>
+                        </a>
+
+                        <a
+                            className="btn btn-outline-light btn-floating m-1"
+                            href="#!"
+                            role="button"
+                        >
+                            <i className="fab fa-linkedin-in"></i>
+                        </a>
+
+                        <a
+                            className="btn btn-outline-light btn-floating m-1"
+                            href="#!"
+                            role="button"
+                        >
+                            <i className="fab fa-github"></i>
+                        </a>
+                    </section>
+                </div>
+
+                <div className="text-center p-3">Develop by BITS</div>
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
